270f95ff38806058b0efbbdb610da1182a331af6
abelino <abelino.romo@gmail.com>
2023-06-22 23:51:30 -0700

    wip

---
+++ package/ti-k3-secdev/ti-k3-secdev.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# ti-k3-secdev
+#
+################################################################################
+
+TI_K3_SECDEV_VERSION = 08.06.00.007
+TI_K3_SECDEV_SITE = https://git.ti.com/cgit/security-development-tools/core-secdev-k3/snapshot
+TI_K3_SECDEV_SOURCE = core-secdev-k3-$(TI_K3_SECDEV_VERSION).tar.gz
+TI_K3_SECDEV_INSTALL_STAGING = NO
+TI_K3_SECDEV_INSTALL_TARGET = NO
+TI_K3_SECDEV_LICENSE = BSD-3-Clause
+
+$(eval $(host-generic-package))
--- package/ti-linux-firmware/ti-linux-firmware.mk
+++ package/ti-linux-firmware/ti-linux-firmware.mk
@@ -8,11 +8,23 @@
 TI_LINUX_FIRMWARE_VERSION = 08.06.00.007
 TI_LINUX_FIRMWARE_SITE = https://git.ti.com/cgit/processor-firmware/ti-linux-firmware/snapshot
 TI_LINUX_FIRMWARE_SOURCE = ti-linux-firmware-$(TI_LINUX_FIRMWARE_VERSION).tar.gz
-TI_LINUX_FIRMWARE_INSTALL_STAGING = YES
 TI_LINUX_FIRMWARE_INSTALL_TARGET = YES
+TI_LINUX_FIRMWARE_INSTALL_IMAGES = YES
+# TI_LINUX_FIRMWARE_RDEPENDENCIES = uboot-tools
 TI_LINUX_FIRMWARE_LICENSE = TI-TFL
-TI_LINUX_FIRMWARE_LICENSE_FILES = licenses/TI-TFL
-# TI_LINUX_FIRMWARE_DEPENDENCIES =
+TI_LINUX_FIRMWARE_LICENSE_FILES = LICENCE.ti-tspa
+TI_LINUX_FIRMWARE_CPE_ID_VENDOR = ti
+TI_LINUX_FIRMWARE_CPE_ID_PRODUCT = u-boot
+TI_LINUX_FIRMWARE_DEPENDENCIES = $(BR2_MAKE_HOST_DEPENDENCY)
+
+UBOOT_PRE_BUILD_HOOKS += TI_LINUX_FIRMWARE_RDEPEND_CMD
+
+define TI_LINUX_FIRMWARE_RDEPEND_CMD
+	$(MAKE) ti-linux-firmware -C $(BASE_DIR)
+endef
+
+# BR2_MAKE_HOST_DEPENDENCY += ti-linux-firmware
+# UBOOT_DEPENDENCIES += ti-linux-firmware
 
 TI_DM_FW_DIR = ti-dm/am62xx
 TI_DM_FW_FILE = ipc_echo_testb_mcu1_0_release_strip.xer5f
@@ -24,17 +36,24 @@ TI_DM_FW_FILE = ipc_echo_testb_mcu1_0_release_strip.xer5f
 
 # Alternative name? am62-main-r5f0_0-fw
 
+define TI_LINUX_FIRMWARE_BUILD_CMDS
+	cd $(@D)
+endef
+
 define TI_LINUX_FIRMWARE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware/$(TI_DM_FW_DIR)
-	mkdir -p $(UBOOT_DIR)/$(TI_DM_FW_DIR)
 
 	cp -a \
 		$(@D)/$(TI_DM_FW_DIR)/${TI_DM_FW_FILE} \
 		$(TARGET_DIR)/lib/firmware/$(TI_DM_FW_DIR)/
+endef
+
+define TI_LINUX_FIRMWARE_INSTALL_IMAGES_CMDS
+	mkdir -p $(BINARIES_DIR)/$(TI_DM_FW_DIR)
 
 	cp -a \
 		$(@D)/$(TI_DM_FW_DIR)/${TI_DM_FW_FILE} \
-		$(UBOOT_DIR)/$(TI_DM_FW_DIR)/
+		$(BINARIES_DIR)/$(TI_DM_FW_DIR)/
 endef
 
 $(eval $(generic-package))
